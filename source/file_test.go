9dce47c7185911216f4e370bb544b1d17bc3cf5d
shuheiktgw <s-kitagawa@mercari.com>
2023-10-04 10:14:24 +0900

    Fix file names

--- source/file_test.go
+++ source/file_test.go
@@ -15,11 +15,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 
 	tests := []struct {
 		desc     string
+		file     string
 		pos      source.Position
 		expected *source.Location
 	}{
 		{
 			desc: "find nothing",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 21,
 				Col:  1,
@@ -28,12 +30,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ServiceDependencyOption name",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 15,
 				Col:  15,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Service: &source.Service{
 					Name: "FederationService",
 					Option: &source.ServiceOption{
@@ -44,12 +47,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ServiceDependencyOption name",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 15,
 				Col:  15,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Service: &source.Service{
 					Name: "FederationService",
 					Option: &source.ServiceOption{
@@ -60,12 +64,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ServiceDependencyOption service",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 15,
 				Col:  32,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Service: &source.Service{
 					Name: "FederationService",
 					Option: &source.ServiceOption{
@@ -76,12 +81,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a MethodOption string timeout",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 19,
 				Col:  47,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Service: &source.Service{
 					Name: "FederationService",
 					Method: &source.Method{
@@ -93,12 +99,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a MethodOption message timeout",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 23,
 				Col:  16,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Service: &source.Service{
 					Name: "FederationService",
 					Method: &source.Method{
@@ -110,12 +117,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ResolverOption method",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 44,
 				Col:  15,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "Post",
 					Option: &source.MessageOption{
@@ -126,12 +134,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ResolverOption request field",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 46,
 				Col:  18,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "Post",
 					Option: &source.MessageOption{
@@ -144,12 +153,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ResolverOption request by",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 46,
 				Col:  28,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "Post",
 					Option: &source.MessageOption{
@@ -162,12 +172,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ResolverOption response name",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 48,
 				Col:  27,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "Post",
 					Option: &source.MessageOption{
@@ -180,12 +191,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ResolverOption response field",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 48,
 				Col:  42,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "Post",
 					Option: &source.MessageOption{
@@ -198,12 +210,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a ResolverOption response autobind",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 48,
 				Col:  60,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "Post",
 					Option: &source.MessageOption{
@@ -216,12 +229,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a FieldOption by",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 38,
 				Col:  50,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "GetPostResponse",
 					Field: &source.Field{
@@ -233,12 +247,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a MessageDependencyOption name",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 35,
 				Col:  15,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "GetPostResponse",
 					Option: &source.MessageOption{
@@ -252,12 +267,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a MessageDependencyOption message",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 35,
 				Col:  32,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "GetPostResponse",
 					Option: &source.MessageOption{
@@ -271,12 +287,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a MessageDependencyOption arg name",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 35,
 				Col:  55,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "GetPostResponse",
 					Option: &source.MessageOption{
@@ -290,12 +307,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a MessageDependencyOption arg by",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 35,
 				Col:  65,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "GetPostResponse",
 					Option: &source.MessageOption{
@@ -309,12 +327,13 @@ func TestFile_FindLocationByPos(t *testing.T) {
 		},
 		{
 			desc: "find a MessageDependencyOption arg inline",
+			file: "service.proto",
 			pos: source.Position{
 				Line: 51,
 				Col:  57,
 			},
 			expected: &source.Location{
-				FileName: filepath.Base("testdata/service.proto"),
+				FileName: "service.proto",
 				Message: &source.Message{
 					Name: "Post",
 					Option: &source.MessageOption{
@@ -330,7 +349,7 @@ func TestFile_FindLocationByPos(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.desc, func(t *testing.T) {
-			path := filepath.Join("testdata", "service.proto")
+			path := filepath.Join("testdata", tc.file)
 			content, err := os.ReadFile(path)
 			if err != nil {
 				t.Fatal(err)
