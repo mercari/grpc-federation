ebbbf3d49c42967ea4da2dd1efa117120993a6f6
pikachu0310 <pikachu13711@gmail.com>
2024-09-06 20:44:20 +0900

    ✨ impl URL and Useinfo functions

--- grpc/federation/cel/url.go
+++ grpc/federation/cel/url.go
@@ -5,6 +5,7 @@ import (
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
+	"github.com/google/cel-go/common/types/traits"
 	"net/url"
 )
 
@@ -108,6 +109,18 @@ func (lib *URLLibrary) CompileOptions() []cel.EnvOption {
 			OverloadFunc(createURLID("parse_string_url"), []*cel.Type{cel.StringType}, URLType,
 				func(_ context.Context, args ...ref.Val) ref.Val {
 					v, err := url.Parse(string(args[0].(types.String)))
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return lib.toURLValue(*v)
+				},
+			),
+		),
+		BindFunction(
+			createURLName("parseRequestURI"),
+			OverloadFunc(createURLID("parseRequestURI_string_url"), []*cel.Type{cel.StringType}, URLType,
+				func(_ context.Context, args ...ref.Val) ref.Val {
+					v, err := url.ParseRequestURI(string(args[0].(types.String)))
 					if err != nil {
 						return types.NewErr(err.Error())
 					}
@@ -236,8 +249,218 @@ func (lib *URLLibrary) CompileOptions() []cel.EnvOption {
 				},
 			),
 		),
+		BindMemberFunction(
+			"escapedFragment",
+			MemberOverloadFunc(createTimeID("escapedFragment_url_string"), URLType, []*cel.Type{}, cel.StringType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return types.String(v.EscapedFragment())
+				},
+			),
+		),
+		BindMemberFunction(
+			"escapedPath",
+			MemberOverloadFunc(createTimeID("escapedPath_url_string"), URLType, []*cel.Type{}, cel.StringType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return types.String(v.EscapedPath())
+				},
+			),
+		),
+		BindMemberFunction(
+			"hostname",
+			MemberOverloadFunc(createTimeID("hostname_url_string"), URLType, []*cel.Type{}, cel.StringType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return types.String(v.Hostname())
+				},
+			),
+		),
+		BindMemberFunction(
+			"isAbs",
+			MemberOverloadFunc(createTimeID("isAbs_url_bool"), URLType, []*cel.Type{}, cel.BoolType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return types.Bool(v.IsAbs())
+				},
+			),
+		),
+		BindMemberFunction(
+			"joinPath",
+			MemberOverloadFunc(createTimeID("joinPath_url_strings_url"), URLType, []*cel.Type{cel.ListType(cel.StringType)}, URLType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+
+					elems := args[0].(traits.Lister)
+					var paths []string
+					for i := types.Int(0); i < elems.Size().(types.Int); i++ {
+						pathElem := elems.Get(i)
+						paths = append(paths, string(pathElem.(types.String)))
+					}
+
+					v.Path, err = url.JoinPath(v.Path, paths...)
+
+					return lib.toURLValue(v)
+				},
+			),
+		),
+		BindMemberFunction(
+			"marshalBinary",
+			MemberOverloadFunc(createTimeID("MarshalBinary_url_bytes"), URLType, []*cel.Type{}, cel.BytesType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+
+					b, err := v.MarshalBinary()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+
+					return types.Bytes(b)
+				},
+			),
+		),
+		BindMemberFunction(
+			"parse",
+			MemberOverloadFunc(createTimeID("parse_url_string_url"), URLType, []*cel.Type{cel.StringType}, URLType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+
+					u, err := v.Parse(string(args[0].(types.String)))
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+
+					return lib.toURLValue(*u)
+				},
+			),
+		),
+		BindMemberFunction(
+			"port",
+			MemberOverloadFunc(createTimeID("port_url_string"), URLType, []*cel.Type{}, cel.StringType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return types.String(v.Port())
+				},
+			),
+		),
+		BindMemberFunction(
+			"query",
+			MemberOverloadFunc(createURLID("query_url_map"), URLType, []*cel.Type{}, cel.MapType(cel.StringType, cel.ListType(cel.StringType)),
+				func(ctx context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+
+					adapter := types.DefaultTypeAdapter
+					queryParams := v.Query()
+					queryMap := map[ref.Val]ref.Val{}
+					for key, values := range queryParams {
+						var valueList []ref.Val
+						for _, val := range values {
+							valueList = append(valueList, types.String(val))
+						}
+						queryMap[types.String(key)] = types.NewStringList(adapter, values)
+					}
+
+					return types.NewRefValMap(adapter, queryMap)
+				},
+			),
+		),
+		BindMemberFunction(
+			"requestURI",
+			MemberOverloadFunc(createTimeID("requestURI_url_string"), URLType, []*cel.Type{}, cel.StringType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return types.String(v.RequestURI())
+				},
+			),
+		),
+		BindMemberFunction(
+			"resolveReference",
+			MemberOverloadFunc(createTimeID("resolveReference_url_url_url"), URLType, []*cel.Type{URLType}, URLType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+
+					r, err := args[0].Value().(*URL).GoURL()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					u := v.ResolveReference(&r)
+
+					return lib.toURLValue(*u)
+				},
+			),
+		),
+		BindMemberFunction(
+			"string",
+			MemberOverloadFunc(createTimeID("string_url_string"), URLType, []*cel.Type{}, cel.StringType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					return types.String(self.Value().(*URL).String())
+				},
+			),
+		),
+		BindMemberFunction(
+			"unmarshalBinary",
+			MemberOverloadFunc(createTimeID("unmarshalBinary_url_bytes_url"), URLType, []*cel.Type{cel.BytesType}, URLType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					var u url.URL
+					err := u.UnmarshalBinary(args[0].(types.Bytes))
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return lib.toURLValue(u)
+				},
+			),
+		),
 
 		// Userinfo functions
+		BindFunction(
+			createURLName("user"),
+			OverloadFunc(createURLID("user_string_userinfo"), []*cel.Type{cel.StringType}, UserinfoType,
+				func(_ context.Context, args ...ref.Val) ref.Val {
+					return lib.toUserinfoValue(string(args[0].(types.String)), "", false)
+				},
+			),
+		),
+		BindFunction(
+			createURLName("userPassword"),
+			OverloadFunc(createURLID("userPassword_string_string_userinfo"), []*cel.Type{cel.StringType, cel.StringType}, UserinfoType,
+				func(_ context.Context, args ...ref.Val) ref.Val {
+					return lib.toUserinfoValue(string(args[0].(types.String)), string(args[1].(types.String)), true)
+				},
+			),
+		),
 		BindMemberFunction(
 			"username",
 			MemberOverloadFunc(createTimeID("username_userinfo_string"), UserinfoType, []*cel.Type{}, cel.StringType,
@@ -277,6 +500,18 @@ func (lib *URLLibrary) CompileOptions() []cel.EnvOption {
 					}
 					_, hasPassword := v.Password()
 					return types.Bool(hasPassword)
+				},
+			),
+		),
+		BindMemberFunction(
+			"string",
+			MemberOverloadFunc(createTimeID("string_userinfo_string"), UserinfoType, []*cel.Type{}, cel.StringType,
+				func(_ context.Context, self ref.Val, args ...ref.Val) ref.Val {
+					v, err := self.Value().(*Userinfo).GoUserinfo()
+					if err != nil {
+						return types.NewErr(err.Error())
+					}
+					return types.String(v.String())
 				},
 			),
 		),
