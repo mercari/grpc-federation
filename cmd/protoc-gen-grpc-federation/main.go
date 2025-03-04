74aff259caebbbb7627e1b38f57c5e61b56895ee
Masaaki Goshima <goccy54@gmail.com>
2025-03-04 18:03:40 +0900

    fix version text with `go install`

--- cmd/protoc-gen-grpc-federation/main.go
+++ cmd/protoc-gen-grpc-federation/main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"os"
 
+	"github.com/jessevdk/go-flags"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/pluginpb"
 
 	"github.com/mercari/grpc-federation/generator"
+	grpcfed "github.com/mercari/grpc-federation/grpc/federation"
+)
+
+type option struct {
+	Version bool `description:"show the version" long:"version" short:"v"`
+}
+
+const (
+	exitOK  = 0
+	exitERR = 1
 )
 
 func main() {
+	_, opt, err := parseOpt()
+	if err != nil {
+		// output error message to stderr by library
+		os.Exit(exitOK)
+	}
+	if opt.Version {
+		fmt.Fprintln(os.Stdout, grpcfed.Version)
+		return
+	}
 	if err := _main(); err != nil {
 		log.Fatal(err)
 	}
@@ -58,4 +79,14 @@ func outputResponse(resp *pluginpb.CodeGeneratorResponse) error {
 		return err
 	}
 	return nil
+}
+
+func parseOpt() ([]string, *option, error) {
+	var opt option
+	parser := flags.NewParser(&opt, flags.Default)
+	args, err := parser.Parse()
+	if err != nil {
+		return nil, nil, err
+	}
+	return args, &opt, nil
 }
