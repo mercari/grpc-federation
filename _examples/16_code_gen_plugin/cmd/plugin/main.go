cfb1ede38c675b6d896c482408b762b5fa159316
Masaaki Goshima <goccy54@gmail.com>
2025-07-04 12:38:21 +0900

    fix test

--- _examples/16_code_gen_plugin/cmd/plugin/main.go
+++ _examples/16_code_gen_plugin/cmd/plugin/main.go
@@ -19,16 +19,16 @@ func main() {
 	}
 	for _, file := range req.GRPCFederationFiles {
 		for _, svc := range file.Services {
-			fmt.Println("service name", svc.Name)
+			fmt.Fprintln(os.Stderr, "service name", svc.Name)
 			for _, method := range svc.Methods {
-				fmt.Println("method name", method.Name)
+				fmt.Fprintln(os.Stderr, "method name", method.Name)
 				if method.Rule != nil {
-					fmt.Println("method timeout", method.Rule.Timeout)
+					fmt.Fprintln(os.Stderr, "method timeout", method.Rule.Timeout)
 				}
 			}
 		}
 		for _, msg := range file.Messages {
-			fmt.Println("msg name", msg.Name)
+			fmt.Fprintln(os.Stderr, "msg name", msg.Name)
 		}
 	}
 	if err := os.WriteFile("resolver_test.go", tmpl, 0o600); err != nil {
