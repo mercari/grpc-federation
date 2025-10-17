16f74b8c7230c232b3131e174d15ea7ae466f882
Masaaki Goshima <goccy54@gmail.com>
2025-10-17 11:57:20 +0900

    add test for plugin

---
+++ grpc/federation/cel/plugin_test.go
@@ -0,0 +1,60 @@
+package cel
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/cel-go/common/types"
+)
+
+func TestPlugin(t *testing.T) {
+	ctx := context.Background()
+	srcPath := filepath.Join("testdata", "plugin", "main.go")
+
+	wasmPath := filepath.Join(t.TempDir(), "test.wasm")
+	cmd := exec.CommandContext(ctx, "go", "build", "-o", wasmPath, srcPath)
+	cmd.Env = append(os.Environ(), "GOOS=wasip1", "GOARCH=wasm")
+	if b, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("failed to build main.go: %q: %v", b, err)
+	}
+	defer os.Remove(wasmPath)
+
+	f, err := os.ReadFile(wasmPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	hash := sha256.Sum256(f)
+	fn := &CELFunction{
+		Name:   "foo",
+		ID:     "foo",
+		Return: types.IntType,
+	}
+	plugin, err := NewCELPlugin(ctx, CELPluginConfig{
+		Name: "test",
+		Wasm: WasmConfig{
+			Reader: bytes.NewBuffer(f),
+			Sha256: hex.EncodeToString(hash[:]),
+		},
+		Functions: []*CELFunction{fn},
+		CacheDir:  "testdata",
+		Capability: &CELPluginCapability{
+			Network: &CELPluginNetworkCapability{},
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := plugin.CreateInstance(ctx, nil); err != nil {
+		t.Fatal(err)
+	}
+	v := plugin.Call(ctx, fn)
+	if int(v.(types.Int)) != 10 {
+		t.Fatalf("failed to get response value: %v", v)
+	}
+}
